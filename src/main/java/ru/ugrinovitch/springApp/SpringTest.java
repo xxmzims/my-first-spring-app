f26c1e023119c65a996cb94d084967390dc143bf
Maksim Ugrinovitch <usmaksim52@gmail.com>
2024-11-21 14:00:35 +0300

    lesson 5. Bean Scope. Prototype and Singleton

--- src/main/java/ru/ugrinovitch/springApp/SpringTest.java
+++ src/main/java/ru/ugrinovitch/springApp/SpringTest.java
@@ -8,10 +8,16 @@ public class SpringTest {
                     "applicationContext.xml");
         // получаем объект из bean-файла
         MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer player2 = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = player == player2;
+        System.out.println(comparison);
+        System.out.println(player);
+        System.out.println(player2);
         player.playMusic();
 
-        System.out.println(player.getName());
-        System.out.println(player.getVolume());
+//        System.out.println(player.getName());
+//        System.out.println(player.getVolume());
 
         context.close();
     }

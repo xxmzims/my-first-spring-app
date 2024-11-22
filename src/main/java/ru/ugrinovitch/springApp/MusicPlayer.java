7c61790a26d04c9137fa6722a15c2be0f39c918b
Maksim Ugrinovitch <usmaksim52@gmail.com>
2024-11-22 11:00:31 +0300

    lesson 8 @Qualifer

--- src/main/java/ru/ugrinovitch/springApp/MusicPlayer.java
+++ src/main/java/ru/ugrinovitch/springApp/MusicPlayer.java
@@ -1,22 +1,21 @@
 package ru.ugrinovitch.springApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-
-    @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    private Music music1;
+    private Music music2;
+@Autowired
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public void playMusic() {
-        System.out.println("Playing " + rockMusic.getSong());
-        System.out.println("Playing " + classicalMusic.getSong());
+        System.out.println("Playing " + music1.getSong() + ", " + music2.getSong());
     }
 }

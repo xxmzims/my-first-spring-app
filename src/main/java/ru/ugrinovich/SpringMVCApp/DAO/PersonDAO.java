e3e318bce438952a2538212e93fb2899997b39d0
Maksim Ugrinovitch <usmaksim52@gmail.com>
2024-11-26 16:08:38 +0300

    CRUD, REST, DAO. Первое CRUD-приложение

---
+++ src/main/java/ru/ugrinovich/SpringMVCApp/DAO/PersonDAO.java
@@ -0,0 +1,31 @@
+package ru.ugrinovich.SpringMVCApp.DAO;
+
+import org.springframework.stereotype.Component;
+import ru.ugrinovich.SpringMVCApp.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private List<Person> people;
+    private static int PEOPLE_COUNT;
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Maks"));
+        people.add(new Person(++PEOPLE_COUNT, "Dima"));
+        people.add(new Person(++PEOPLE_COUNT, "Dasha"));
+        people.add(new Person(++PEOPLE_COUNT, "Vitya"));
+    }
+    public List<Person> index(){
+        return people;
+    }
+    public Person show(int id){
+        for(Person person: people){
+            if (person.getId() == id){
+                return person;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/ru/ugrinovich/SpringMVCApp/controllers/PeopleController.java
@@ -0,0 +1,35 @@
+package ru.ugrinovich.SpringMVCApp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.ugrinovich.SpringMVCApp.DAO.PersonDAO;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        // Получаем всех людей из DAO и передадим на отображение в представление
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        // Получим одного человека по id из DAO b передадим на отображение в представление
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+}
